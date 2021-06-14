e59ad5d32a4d9467cb8bffc77e9f0cc72bcd1639
Zoryana <zfedorovska@gmail.com>
2021-06-14 21:52:44 +0300

    added db connection

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ const knex = require('knex');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-sinuous-07846',
-		port: '5433',
-		user: 'postgres',
-		password: 'admin',
-		database: 'users'
+		host: process.env.DATABASE_URL,
+		ssl: true,
 	}
 });
 
