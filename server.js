1a8b04a50854658e7ad86d089e88d6d12e856bef
Zoryana <zfedorovska@gmail.com>
2021-06-14 17:13:57 +0300

    added port from env

--- server.js
+++ server.js
@@ -108,6 +108,6 @@ app.delete('/del',(req,res) => {
 		.catch(err => res.status(400).json('unable to get entries'))	
 		})
 
-app.listen(3000, ()=> {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`app is running on port {process.env.PORT}`);
 })
